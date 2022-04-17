55f84ed0f8fe628e643186aae61a57ab4d3a1e2e
Ganesh Babu Jayaraman <ganesh@arkainsure.com>
2022-04-18 04:00:54 +0530

    Added Swagger

--- src/main/java/com/example/simpleuserserv2/Simpleuserserv2Application.java
+++ src/main/java/com/example/simpleuserserv2/Simpleuserserv2Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc // Required for Swagger-UI
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.example")
 public class Simpleuserserv2Application {
